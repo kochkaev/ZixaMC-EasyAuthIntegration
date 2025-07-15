282fad1869d5943ac3ada888533e33d588eb73c8
dmitrii-kochkaev <edukisel@yandex.ru>
2025-07-15 12:12:21 +0700

    Bugfix

--- src/main/kotlin/ru/kochkaev/zixamc/easyauthintegration/ZixaMCEasyAuthIntegration.kt
+++ src/main/kotlin/ru/kochkaev/zixamc/easyauthintegration/ZixaMCEasyAuthIntegration.kt
@@ -7,23 +7,29 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
 import ru.kochkaev.zixamc.api.Initializer
 import ru.kochkaev.zixamc.api.config.ConfigManager
 import ru.kochkaev.zixamc.api.telegram.ServerBot
+import ru.kochkaev.zixamc.api.telegram.ServerBotEvents
 
 class ZixaMCEasyAuthIntegration: ModInitializer {
 
     override fun onInitialize() {
         ConfigManager.registerConfig(Config)
-        ServerBot.bot.registerCallbackQueryHandler(/*"easyauth", EasyAuthIntegration.EasyAuthCallbackData::class.java,*/AuthManager::onTelegramCallbackQuery)
         ServerLifecycleEvents.SERVER_STARTED.register { server ->
-            ServerPlayConnectionEvents.DISCONNECT.register { handler, _ ->
-                Initializer.coroutineScope.launch {
-                    AuthManager.onLeave(handler.player.nameForScoreboard)
-                }
+            ServerBot.bot.registerCallbackQueryHandler(/*"easyauth", EasyAuthIntegration.EasyAuthCallbackData::class.java,*/AuthManager::onTelegramCallbackQuery)
+        }
+        ServerPlayConnectionEvents.DISCONNECT.register { handler, _ ->
+            Initializer.coroutineScope.launch {
+                AuthManager.onLeave(handler.player.nameForScoreboard)
             }
-            EasyAuthCustomEvents.UPDATE_PLAYER_AUTHENTICATED_EVENT.register { authenticated, player ->
-                Initializer.coroutineScope.launch {
-                    if (!authenticated) AuthManager.onJoin(player)
+        }
+        EasyAuthCustomEvents.UPDATE_PLAYER_AUTHENTICATED_EVENT.register { authenticated, player ->
+            Initializer.coroutineScope.launch {
+                if (!authenticated) AuthManager.onJoin(player)
 //                   else onLeave(player)}
-                }
+            }
+        }
+        ServerPlayConnectionEvents.JOIN.register { handler, sender, server ->
+            Initializer.coroutineScope.launch {
+                if (!AuthManager.isAuthenticated(handler.player)) AuthManager.onJoin(handler.player)
             }
         }
     }
